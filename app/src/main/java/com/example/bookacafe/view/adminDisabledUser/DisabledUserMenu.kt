157925f822daa927bb07b1f205d4f1d580c24c46
Feliciana Gunadi <isabel.feli27gunadi@gmail.com>
2023-03-25 09:57:00 +0700

    Admin Disabled User Menu Layout

---
+++ app/src/main/java/com/example/bookacafe/view/adminDisabledUser/DisabledUserMenu.kt
@@ -0,0 +1,47 @@
+package com.example.bookacafe.view.adminDisabledUser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bookacafe.R
+import com.example.bookacafe.databinding.AdminDisabledUserBinding
+import com.example.bookacafe.model.MemberDummy
+
+class DisabledUserMenu: AppCompatActivity() {
+    private lateinit var binding: AdminDisabledUserBinding
+    private val list = ArrayList<MemberDummy>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AdminDisabledUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvUsers.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    fun getListHeroes(): ArrayList<MemberDummy> {
+        val dataName = resources.getStringArray(R.array.userName)
+        val dataDescription = resources.getStringArray(R.array.userOrderHistory)
+        val dataPhoto = resources.getStringArray(R.array.user_profile)
+
+        val listHero = ArrayList<MemberDummy>()
+        for (position in dataName.indices) {
+            val hero = MemberDummy(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        binding.rvUsers.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListUserAdapter(list)
+        binding.rvUsers.adapter = listHeroAdapter
+    }
+}
