b82da020631818733b077f484442fd6e7ec12134
Silvi Prisillia <silviprisillia4@yahoo.com>
2023-03-29 08:38:08 +0700

    Menu Book & Menu Table Layout

--- app/src/main/java/com/example/bookacafe/MainActivity.kt
+++ app/src/main/java/com/example/bookacafe/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.bookacafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.example.bookacafe.view.MenuTable
+import com.example.bookacafe.view.MenuBook
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        //testing dari feli :)
+    override fun onClick(v: View) {
+
     }
 }
