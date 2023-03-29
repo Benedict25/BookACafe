e2aa6bae164caeacfbd28c8fc58ab030f24d462c
Silvi Prisillia <silviprisillia4@yahoo.com>
2023-03-29 08:32:54 +0700

    Menu Book & Menu Table Layout

--- app/src/main/java/com/example/bookacafe/model/Book.kt
+++ app/src/main/java/com/example/bookacafe/model/Book.kt
@@ -1,12 +1,16 @@
 package com.example.bookacafe.model
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-class Book (
-    bookId: Int,
-    title: String,
-    author: String,
-    genre: String,
-    synopsis: String,
-    imagePath: String,
-    stock: Int,
-    status: ItemTypeEnum
-)
+@Parcelize
+class Book(
+    //var bookId: Int
+    var title: String,
+    var author: String,
+    //var genre: String
+    //var synopsis: String
+    var imagePath: Int,
+    var stock: Int
+    //var status: ItemTypeEnum
+) : Parcelable
+
--- app/src/main/java/com/example/bookacafe/view/MenuTable.kt
+++ app/src/main/java/com/example/bookacafe/view/MenuTable.kt
@@ -1,5 +1,75 @@
 package com.example.bookacafe.view
 
-class MenuTable {
-    fun ShowTables() {}
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bookacafe.R
+
+class MenuTable : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnTableA1: Button
+    private lateinit var btnTableA2: Button
+    private lateinit var btnTableA3: Button
+    private lateinit var btnTableA4: Button
+    private lateinit var btnTableB1: Button
+    private lateinit var btnTableB2: Button
+    private lateinit var btnTableB3: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_table)
+
+        btnTableA1 = findViewById(R.id.btn_table_a1)
+        btnTableA1.setOnClickListener(this)
+
+        btnTableA2 = findViewById(R.id.btn_table_a2)
+        btnTableA2.setOnClickListener(this)
+
+        btnTableA3 = findViewById(R.id.btn_table_a3)
+        btnTableA3.setOnClickListener(this)
+
+        btnTableA4 = findViewById(R.id.btn_table_a4)
+        btnTableA4.setOnClickListener(this)
+
+        btnTableB1 = findViewById(R.id.btn_table_b1)
+        btnTableB1.setOnClickListener(this)
+
+        btnTableB2 = findViewById(R.id.btn_table_b2)
+        btnTableB2.setOnClickListener(this)
+
+        btnTableB3 = findViewById(R.id.btn_table_b3)
+        btnTableB3.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        showTables(v)
+    }
+
+    private fun showTables(v: View) {
+        when (v.id) {
+            R.id.btn_table_a1 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_a2 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_a3 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_a4 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_b1 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_b2 -> {
+                TODO("Button clicked")
+            }
+            R.id.btn_table_b3 -> {
+                TODO("Button clicked")
+            }
+        }
+    }
+
+
 }
