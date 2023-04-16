908e966abcc3d9b1eb49a9252aa03cc69194ca91
nicholasrussel <nicholasrussel1234@gmail.com>
2023-04-16 13:12:51 +0700

    Menambah text username,total cart,checkin

--- app/src/main/java/com/example/bookacafe/view/FirstFragment.kt
+++ app/src/main/java/com/example/bookacafe/view/FirstFragment.kt
@@ -1,5 +1,3 @@
-package com.example.bookacafe.view
-
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,18 +9,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.example.bookacafe.R
+import com.example.bookacafe.controller.CartControllers
+import com.example.bookacafe.controller.HomePageControllers
+import com.example.bookacafe.view.*
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
-class FirstFragment:Fragment(R.layout.fragment_first) {
-    private lateinit var viewPager: ViewPager
-    private lateinit var viewPagerAdapter: ViewPagerAdapter
-    private lateinit var imageList: List<Int>
-    private lateinit var timer: Timer
-    private lateinit var nameTextView: TextView
-    private var currentPosition = 0
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
+    lateinit var timer: Timer
+    lateinit var timeLeftTextView: TextView
+    lateinit var nameTextView: TextView
+    var currentPosition = 0
+    val control: CartControllers = CartControllers()
+    val controlHome: HomePageControllers = HomePageControllers()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,9 +42,15 @@ class FirstFragment:Fragment(R.layout.fragment_first) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val myClickableText = view.findViewById<TextView>(R.id.seeDetails)
         val clickableImage: ImageButton = view.findViewById(R.id.imageProfile)
-        clickableImage.setOnClickListener{
+
+        setCheckedIn(controlHome.getTime(), view)
+        setTotal(control.getTotalForCart(), view)
+        setTimeDuration(view)
+
+        clickableImage.setOnClickListener {
             val intent = Intent(context, MenuProfile::class.java)
             startActivity(intent)
         }
@@ -46,20 +59,6 @@ class FirstFragment:Fragment(R.layout.fragment_first) {
             intent.putExtra("transaction_id", "TR20230211-001")
             startActivity(intent)
         }
-//        myClickableText.setOnClickListener {
-//
-//            val mCategoryFragment = SecondFragment()
-//            val mFragmentManager = parentFragmentManager as FragmentManager
-//            mFragmentManager
-//                .beginTransaction()
-//                .replace(
-//                    R.id.fragment_container,
-//                    mCategoryFragment,
-//                    SecondFragment::class.java.simpleName
-//                )
-//                .addToBackStack(null)
-//                .commit()
-//        }
 
         viewPager = view.findViewById(R.id.idViewPager)
 
@@ -89,10 +88,12 @@ class FirstFragment:Fragment(R.layout.fragment_first) {
         var acct: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(requireActivity())
 
         // Set data ; ambil dari data yang login sekarang
-        if (acct != null){
+        if (acct != null) {
             var activeName: String = acct.displayName.toString()
-            var displayName = "Welcome \n $activeName!"
+            var displayName = "Welcome \n ${activeName}"
             nameTextView.setText(displayName)
+        } else {
+            setName(controlHome.getName(), view)
         }
     }
 
@@ -102,6 +103,64 @@ class FirstFragment:Fragment(R.layout.fragment_first) {
         timer.cancel()
     }
 
+    fun setTotal(total: Int, view: View) {
+        var cartTotal: TextView = view.findViewById(R.id.cartShortCut)
+        if (total != 0) {
+            cartTotal.text = "Rp$total,-"
+        } else {
+            cartTotal.text = "No order yet"
+        }
+
+    }
+
+    fun setName(name: String, view: View) {
+        var homePageUserName: TextView = view.findViewById(R.id.homeUserName)
+        homePageUserName.text = "Welcome\n ${name}"
+    }
+
+    fun setCheckedIn(time: String, view: View) {
+        var timeCheckedIn: TextView = view.findViewById(R.id.textCheckedIn)
+
+        if (time != "") {
+            timeCheckedIn.text = "Checked In ${time}"
+        } else {
+            timeCheckedIn.text = "No tables yet"
+        }
+    }
+
+    fun setTimeDuration(view: View) {
+        timeLeftTextView = view.findViewById(R.id.textTimeLeft)
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val timeCheckIn = controlHome.getTime()
+
+        if(timeCheckIn == ""){
+            timeLeftTextView.text = "Been here for N/A"
+
+        } else {
+            // get localtime
+            val calendar = Calendar.getInstance()
+            val localTime = calendar.time
+
+            val localTimeAsString = dateFormat.format(localTime)
+            val date = dateFormat.parse(localTimeAsString)
+            val dateCheckIn = dateFormat.parse(timeCheckIn)
+
+            // mengubah selisih waktu menjadi jam dan menit
+            val diffTime = date.time - dateCheckIn.time
+            val hours = TimeUnit.MILLISECONDS.toHours(diffTime)
+            val minutes = TimeUnit.MILLISECONDS.toMinutes(diffTime) % 60
+
+
+            // format hasil menjadi "hh:mm"
+            val result = String.format("%02d:%02d", hours, minutes)
+
+            timeLeftTextView.text = "Been here for " + result
+        }
+
+
+
+
+
+    }
+
 }
-
-
